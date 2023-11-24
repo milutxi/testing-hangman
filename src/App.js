321c237c97fe9da5e49fd2c59217dcfbe73fe9a9
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-11-24 09:35:12 +0100

    added start button and words component to here

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
+import Words from './components/Words'; 
 import './App.css';
 
+const wordToGuess = [
+  "cheer", "table", "beach", "green", "drive", "hello", "admit", "earth",
+  "image", "brown", "mouse", "house", "water", "drink"
+];
+
 function App() {
+  const [randomWord, setRandomWord] = useState("");
+
+  const getRandomWord = () => {
+    const randomIndex = Math.floor(Math.random() * wordToGuess.length);
+    const selectedWord = wordToGuess[randomIndex];
+    setRandomWord(selectedWord);
+  };
+
   return (
     <div className="App">
       <Header />
+      <button onClick={getRandomWord}>Start game</button>
+      {randomWord && <Words word={randomWord} />} 
     </div>
   );
 }
 
 export default App;
+
+
