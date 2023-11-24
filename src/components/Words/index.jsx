486b1310eaeff6265810db46121b8813ece86cf8
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-11-24 09:29:20 +0100

    added testing for word

--- src/components/Words/index.jsx
+++ src/components/Words/index.jsx
@@ -1,19 +1,16 @@
 const wordToGuess = ["cheer", "table", "beach", "green", "drive", "hello", "admit", "earth", "image", "brown", "mouse", "house", "water", "drink"];
 const randomWord = wordToGuess[Math.floor(Math.random() * wordToGuess.length)];
 
-const Words = () => {
-    return (
-      <div data-testid="hangman-word">
-        {/* take the word, create individual characters, write with map */}
-        {randomWord.split('').map((letter, index) => (
-          <span key={index}>
-            
-              {letter}
-            
-          </span>
-        ))}
-      </div>
-    )
-  }
-  
-  export default Words
+
+function Words({ word }) {
+  return (
+    <div data-testid="hangman-word">
+      {word && typeof word === 'string' && word.split('').map((letter, index) => (
+        <span key={index}>{letter}</span>
+      ))}
+    </div>
+  );
+}
+
+export default Words;
+
--- src/components/Words/words.test.js
+++ src/components/Words/words.test.js
@@ -1,15 +1,40 @@
-import {render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Words from '.';
 
+const wordToGuess = [
+    "cheer", "table", "beach", "green", "drive", "hello", "admit", "earth",
+    "image", "brown", "mouse", "house", "water", "drink"
+  ];
+
+Math.random = () => 0.5;
+//line 4-9 get deleted
 test("The component is being rendered without error", () => {
-    render (<Words randomWord={} />);
+  render(<Words />);
+});
 
-})
+test("Display the right letters of the guess word", () => {
+    const selectedWord = "cheer"; 
+  
+    const { getByTestId } = render(<Words word={selectedWord} />);
+  
+    const displayedWord = getByTestId('hangman-word').textContent;
+  
+    expect(displayedWord).toBe(selectedWord);
+  });
+  
+test("Display a different word to guess", () => {
 
-test("Display the right letter", () => {
-    render (<Words />);
-    const displayedWord = screen.getByTestId('hangman-word').textContent;
-    expect(displayedWord).toBe('table')
-})
+  const randomIndex = Math.floor(Math.random() * wordToGuess.length);
+  const word = wordToGuess[randomIndex];
+
+  render(<Words word={word} />);
+
+  const displayedWord = screen.getByTestId('hangman-word').textContent;
+  expect(displayedWord).toBe(word);
+});
+
+
+  
+
 
 
